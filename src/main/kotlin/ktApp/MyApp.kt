35de1fe0c1737a0852125e6e43f8e87c521ecc45
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-10-03 15:04:13 -0300

    Basiscs of listings Brazilian States and countries working.

--- src/main/kotlin/ktApp/MyApp.kt
+++ src/main/kotlin/ktApp/MyApp.kt
@@ -3,12 +3,17 @@ package com.github.OpenICP_BR.ktApp
 import com.github.OpenICP_BR.ktApp.views.MainView
 import javafx.application.Application
 import tornadofx.App
+import java.io.File
+import java.nio.file.Paths
 
-class MyApp: App(MainView::class, Styles::class)
+class MyApp: App(MainView::class)
 
 /**
  * The main method is needed to support the mvn jfx:run goal.
  */
 fun main(args: Array<String>) {
+    val path = Paths.get("").toAbsolutePath().toString()
+    println("Working Directory = $path")
+    println(File("res/main.fxml").exists())
     Application.launch(MyApp::class.java, *args)
 }
--- src/main/kotlin/ktApp/views/MainView.kt
+++ src/main/kotlin/ktApp/views/MainView.kt
@@ -4,24 +4,22 @@ import com.github.OpenICP_BR.ktApp.Styles
 import javafx.scene.control.Alert.AlertType.INFORMATION
 import tornadofx.*
 import com.github.OpenICP_BR.ICP.TESTING_ROOT_CA_SUBJECT
+import javafx.scene.control.ComboBox
+import javafx.scene.control.TabPane
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.scene.control.ChoiceBox
 
-class MainView : View("Hello TornadoFX") {
-    override val root = borderpane {
-        addClass(Styles.welcomeScreen)
-        top {
-            stackpane {
-                label(title).addClass(Styles.heading)
-            }
-        }
-        center {
-            stackpane {
-                addClass(Styles.content)
-                button("Click me") {
-                    setOnAction {
-                        alert(INFORMATION, "Well done!", "You clicked me! " +TESTING_ROOT_CA_SUBJECT)
-                    }
-                }
-            }
-        }
+
+class MainView : View() {
+    override val root : TabPane by fxml("/main.fxml")
+    val ufs : ComboBox<String> by fxid("SignPanelStates")
+    val countries : ComboBox<String> by fxid("SignPanelCountries")
+
+    override fun onBeforeShow() {
+        super.onBeforeShow()
+        // Add states and Countries
+        ufs.setItems(FXCollections.observableArrayList(this.messages["BR.States"].split(";")))
+        countries.setItems(FXCollections.observableArrayList(this.messages["INT.Countries"].split(";")))
     }
 }
