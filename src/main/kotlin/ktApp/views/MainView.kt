9e222a9017b121d077e19c6879006120bbce027f
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-10-05 09:13:38 -0300

    Improved UI.

--- src/main/kotlin/ktApp/views/MainView.kt
+++ src/main/kotlin/ktApp/views/MainView.kt
@@ -1,26 +1,39 @@
 package com.github.OpenICP_BR.ktApp.views
 
-import com.github.OpenICP_BR.ktApp.Styles
-import javafx.scene.control.Alert.AlertType.INFORMATION
 import tornadofx.*
-import com.github.OpenICP_BR.ICP.TESTING_ROOT_CA_SUBJECT
 import javafx.scene.control.ComboBox
 import javafx.scene.control.TabPane
 import javafx.collections.FXCollections
-import javafx.collections.ObservableList
-import javafx.scene.control.ChoiceBox
+import javafx.scene.control.Label
+import javafx.scene.control.ProgressBar
 
 
 class MainView : View() {
     override val root : TabPane by fxml("/main.fxml")
-    val ufs : ComboBox<String> by fxid("SignPanelStates")
-    val countries : ComboBox<String> by fxid("SignPanelCountries")
+    val state : ComboBox<String> by fxid("SignPanelState")
+    val country : ComboBox<String> by fxid("SignPanelCountry")
+    val adv_types : ComboBox<String> by fxid("AdvPanelType")
+    val sign_bar : ProgressBar by fxid("SignPanelProgressBar")
+    val sign_status : Label by fxid("SignPanelStatus")
+    val adv_bar : ProgressBar by fxid("AdvPanelProgressBar")
+    val adv_status : Label by fxid("AdvPanelStatus")
 
     override fun onBeforeShow() {
         super.onBeforeShow()
         // Add states and Countries
-        ufs.setItems(FXCollections.observableArrayList(this.messages["BR.States"].split(";")))
-        countries.setItems(FXCollections.observableArrayList(this.messages["INT.Countries"].split(";")))
-        countries.selectionModel.select(32)
+        state.items = FXCollections.observableArrayList(this.messages["BR.States"].split(";"))
+        country.items = FXCollections.observableArrayList(this.messages["INT.Countries"].split(";"))
+        // Select Brazil
+        country.selectionModel.select(32)
+
+        // Add certificate types
+        adv_types.items = FXCollections.observableArrayList(this.messages["Adv.Types"].split(";"))
+
+        // Hide progress bars and statuses
+        sign_bar.hide()
+        sign_status.hide()
+        adv_bar.hide()
+        adv_status.hide()
+
     }
 }
