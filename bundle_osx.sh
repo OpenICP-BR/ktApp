3ce3449131f6d24d39f85384cd36c3a7c4c4e2a9
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-10-11 21:11:34 -0300

    Improved macOS bundle script.

--- bundle_osx.sh
+++ bundle_osx.sh
@@ -7,36 +7,51 @@
 # BUG: adding a JRE does not work
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 VERSION=$1
 JDK=/Library/Java/JavaVirtualMachines/jdk-10.jdk
 
+fail () {
+    echo -e "${RED}Got error code $? from previous command.${NC}"
+    exit -1
+}
+
+if [[ -z "${VERSION}" ]]; then
+    echo -e "${RED}You MUST specify the version number${NC}"
+    exit -2
+fi
+if [[ "$2" ]]; then
+    JDK=$2
+fi
+echo -e "Using JDK on: ${JDK}"
+
 # Get on the correct directory
 cd target
 
 # Prepare JRE for bundling
-mkdir -p jdk-10.jdk/Contents/Home/jre/
-ln -s ${JDK}/Contents/MacOS jdk-10.jdk/Contents
-ln -s ${JDK}/Contents/Home/bin jdk-10.jdk/Contents/Home/jre/
-ln -s ${JDK}/Contents/Home/conf jdk-10.jdk/Contents/Home/jre/
-ln -s ${JDK}/Contents/Home/jmods jdk-10.jdk/Contents/Home/jre/
-ln -s ${JDK}/Contents/Home/lib jdk-10.jdk/Contents/Home/jre/
-cp ${JDK}/Info.plist jdk-10.jdk/Contents/
+rm -vrf jdk-10.jdk
+mkdir -p jdk-10.jdk/Contents/Home/jre/ || fail
+ln -s ${JDK}/Contents/MacOS jdk-10.jdk/Contents || fail
+ln -s ${JDK}/Contents/Home/bin jdk-10.jdk/Contents/Home/jre/ || fail
+ln -s ${JDK}/Contents/Home/conf jdk-10.jdk/Contents/Home/jre/ || fail
+ln -s ${JDK}/Contents/Home/jmods jdk-10.jdk/Contents/Home/jre/ || fail
+ln -s ${JDK}/Contents/Home/lib jdk-10.jdk/Contents/Home/jre/ || fail
+cp ${JDK}/Info.plist jdk-10.jdk/Contents/ || fail
 
-rm -rf "OpenICP-BR.app"
-jar2app "ktApp-${VERSION}.jar" -n "OpenICP-BR" -i ../other_res/osx/logo.icns -s "${VERSION}" -v "${VERSION}" -r jdk-10.jdk || 
-exit -1
-echo -e "${GREEN}Generated .app bunlde ${NC}"
+rm -rf "OpenICP-BR.app" || fail
+jar2app "ktApp-${VERSION}.jar" -n "OpenICP-BR" -i ../other_res/osx/logo.icns -s "${VERSION}" -v "${VERSION}" -r jdk-10.jdk || fail
+echo -e "${GREEN}Generated .app bundle ${NC}"
 
-mkdir OpenICP-BR.app/Contents/Java/lib || exit -1
-cp -arv lib/* OpenICP-BR.app/Contents/Java/lib || exit -1
-echo -e "${GREEN}Copied java libraries to .app bunlde ${NC}" || exit -1
+mkdir OpenICP-BR.app/Contents/Java/lib || fail
+cp -arv lib/* OpenICP-BR.app/Contents/Java/lib || fail
+echo -e "${GREEN}Copied java libraries to .app bunlde ${NC}" || fail
 
 rm -rf dmg
 mkdir dmg
-ln -s /Applications ./dmg/Aplicativos || exit -1
-cp -r OpenICP-BR.app ./dmg/ || exit -1
-create-dmg OpenICP-BR.app --overwrite || exit -1
-rm ./dmg/Aplicativos || exit -1
+ln -s /Applications ./dmg/Aplicativos || fail
+cp -r OpenICP-BR.app ./dmg/ || fail
+create-dmg OpenICP-BR.app --overwrite || fail
+rm ./dmg/Aplicativos || fail
 echo -e "${GREEN}Created DMG file${NC}"
