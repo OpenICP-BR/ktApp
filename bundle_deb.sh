7fa53244a3174d6695a641fe7d8797ea6faf67eb
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-10-11 21:56:46 -0300

    .deb file generation

--- bundle_deb.sh
+++ bundle_deb.sh
@@ -26,12 +26,22 @@ mkdir -pv target/deb/DEBIAN || fail
 mkdir -pv target/deb/usr/bin || fail
 mkdir -pv target/deb/usr/share/applications || fail
 mkdir -pv target/deb/usr/share/openicp-br/lib || fail
+for RES in scalable 16x16 32x32 48x48 64x64 128x128 256x256; do
+    mkdir -pv target/deb/usr/share/icons/hicolor/${RES}/apps || fail
+done
 echo -e "${GREEN}Cleaned previous files and created structure ${NC}"
 
 # Copy files
-cp -v target/ktApp-${VERSION}.jar target/deb/usr/share/openicp-br/ || fail
+cp -v target/ktApp-${VERSION}.jar target/ktApp.jar || fail
+cp -v other_res/linux/openicpbr target/deb/usr/bin/ || fail
+chmod +x target/deb/usr/bin/openicpbr || fail
+cp -v target/ktApp.jar target/deb/usr/share/openicp-br/ || fail
 cp -v target/lib/*.jar target/deb/usr/share/openicp-br/lib/ || fail
 cp -v other_res/linux/com.github.OpenICP_BR.ktApp.desktop  target/deb/usr/share/applications || fail
+cp -v src/main/resources/logo.svg target/deb/usr/share/icons/hicolor/scalable/apps/com.github.OpenICP_BR.ktApp.svg || fail
+for RES in 16x16 32x32 48x48 64x64 128x128 256x256; do
+    cp -v src/main/resources/logo-${RES}.png target/deb/usr/share/icons/hicolor/${RES}/apps/com.github.OpenICP_BR.ktApp.png || fail
+done
 echo -e "${GREEN}Copied files ${NC}"
 
 # Get total file size
