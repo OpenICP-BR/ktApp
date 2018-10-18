b49b4dc762d3b8e41f9976fb20251d3fa5af6813
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-10-18 01:00:40 -0300

    Icon working.

--- src/main/kotlin/ktApp/views/MainView.kt
+++ src/main/kotlin/ktApp/views/MainView.kt
@@ -1,9 +1,17 @@
 package com.github.OpenICP_BR.ktApp.views
 
+import javafx.scene.image.Image
 import tornadofx.*
 import javafx.scene.layout.VBox
 import main.kotlin.ktApp.views.AdvView
 import main.kotlin.ktApp.views.SignView
+import java.nio.file.Files
+import java.util.stream.Stream
+import java.nio.file.Paths
+import java.nio.file.FileSystems
+import java.nio.file.Path
+import java.util.*
+
 
 class MainView : View() {
     override val root : VBox by fxml("/main.fxml")
@@ -17,6 +25,34 @@ class MainView : View() {
         signView.master = this
         advView.fxmlLoader = this.fxmlLoader
         advView.master = this
+
+        // Set icon
+        var res = MainView::class.java.getResourceAsStream("/logo.svg")
+        this.primaryStage.icons.add(Image(res));
+        res = MainView::class.java.getResourceAsStream("/logo-64x64.png")
+        this.primaryStage.icons.add(Image(res));
+        res = MainView::class.java.getResourceAsStream("/logo-48x48.png")
+        this.primaryStage.icons.add(Image(res));
+        res = MainView::class.java.getResourceAsStream("/logo-32x32.png")
+        this.primaryStage.icons.add(Image(res));
+        res = MainView::class.java.getResourceAsStream("/logo-16x16.png")
+        this.primaryStage.icons.add(Image(res));
+
+        // List all reource files
+        val uri = MainView::class.java!!.getResource("").toURI()
+        val myPath: Path
+        if (uri.getScheme().equals("jar")) {
+            val fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap<String, Object>())
+            myPath = fileSystem.getPath("")
+        } else {
+            println(uri)
+            myPath = Paths.get(uri)
+        }
+        val walk = Files.walk(myPath, 1)
+        val it = walk.iterator()
+        while (it.hasNext()) {
+            System.out.println(it.next())
+        }
     }
 
     override fun onBeforeShow() {
